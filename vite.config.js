3482513fd09d4f6353c068495b74381cda0e58b0
Gogi24 <goltrandmissou@gmail.com>
2026-09-01 11:35:57 +0100

    remove base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/goltrand-boukoumou.github.io/',
+  base: '/',
   plugins: [react()],
 })
